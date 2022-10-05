516bb4621d526f1beb2a467c8412f00c5de7bf79
WiseWill <bmendola@newtech.coppellisd.com>
2022-10-05 17:49:29 -0500

    hello

--- src/lab/ArrayAnalysis.java
+++ src/lab/ArrayAnalysis.java
@@ -1,5 +1,8 @@
 package lab;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 /*
  * See the link for lab explanation
  * https://docs.google.com/document/d/1QGhcNy-QOFT-J1rOHgeXKv81b5yz0LfEpL_Dl3PPff8/edit#
@@ -12,15 +15,27 @@ public class ArrayAnalysis {
 	
 	//#1
 	public static int[][] createRandom(int row, int column, int min, int max){
-		
-		
-		return null;
+		int [][] randA = new int [row][column];
+		//creating a loop that will fill all with random numbers it will be a nested loop
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < column;j++){
+				randA [i][j] = (int)(Math.random()*(max-min)+min);
+				
+				
+			}
+		}
+		return randA;
 	}
 	
 	
 	//#2
-	public static void printArray(int[][] arr)  {
-		
+	public void printArray(int[][] arr)  {
+		for (int i = 0;i < arr.length;i++) {
+			for (int j = 0;j < arr[0].length;j++) {
+				System.out.printf("%4d ", arr[i][j]);
+			}
+			System.out.println();
+		}
 		
 	
 	}
@@ -28,67 +43,125 @@ public class ArrayAnalysis {
 	
 	//#3
 	public static int sumTotal(int[][] arr) {
+		//for the length and the width to cycle through
+		int total = 0;
+		for(int i = 0; i < arr.length;i++) {
+			for (int j = 0; j < arr[i].length;j++) {
+				total += arr[i][j];
+			}
+			
+		}
 		
-		
-		return 0;
+		return total;
 	}
 	
 	
 	//#4
 	public static double average(int[][] arr) {
+	//same as last but with avraging at end
 		
 		
 		
-		return 0;
+		return (double) sumTotal(arr) / (arr.length * arr[0].length);
 	}
 	
 	
 	//#5
 	public static int findMin(int[][] arr) {
+		int min = 999999999;
 		
+		for(int i = 0; i < arr.length;i++) {
+			for (int j = 0; j < arr[0].length;j++) {
+				if (min > arr[i][j]) {
+					min = arr[i][j];
+				}
+			}
+		}
 		
-		return 0;
+		return min;
 	}
 	
 	
 	//#6
 	public static int findMax(int[][] arr) {
+		int max = -999999999;
+		
+		for(int i = 0; i < arr.length;i++) {
+			for (int j = 0; j < arr[0].length;j++) {
+				if (max < arr[i][j]) {
+					max = arr[i][j];
+				}
+			}
+		}
+		
+		return max;
 		
 		
-		return 0;
 	}
 	
 	
 	//#7
 	public static int[][] diffArrays(int[][] arr1, int[][] arr2){
-		
-		
-		
-		return null;
+		//checking parameters
+		if (arr1.length != arr2.length) {
+			return null;
+		}
+		if (arr1[0].length != arr2[0].length) {
+			return null;
+		}
+		//making it loop through each value and find the corosponding valuye and subtract.
+		int [][] arr3 = new int [arr1.length][arr1[0].length];
+		for (int i = 0; i < arr1.length;i++) {
+			for (int j = 0; j < arr1[0].length;j++) {
+				arr3[i][j] = arr1[i][j]-arr2[i][j];
+			}
+		}
+		return arr3;
 	}
 	
 	
 	//#8
 	public static int[][] transposeRow(int[][] arr1, int dist){
+		//making a save row
+	int[] save = new int[arr1[0].length];
+	
+		//for the times to move
 		
+		for (int i = 0; i < dist; i++) {
+			//save first than move down
+			save = arr1[arr1.length-1];
+			for (int j = arr1.length-1; j > 0; j--) {
+				
+				arr1[j]=arr1[j-1];
+				
+			}
+			arr1[0] = save;
 		
-		return null;
+	
+		}
+		
+		//arr1[0] = save;
+		return arr1;
 	}
 	
 	
 	//#9
 	public static int sum3D(int[][][] arr) {
+		int total = 0;
+		for (int i = 0; i < arr.length;i++ ) {
+			total += sumTotal(arr[i]);
+		}
 		
-		
-		return 0;
+		return total;
 	}
 	
 	
 	//#10
 	public static double average3D(int[][][] arr) {
+		double avg = (int)sum3D(arr)/(arr.length* arr[0].length*arr[0][0].length);
 		
 		
-		return 0;
+		return avg;
 	}
 	
 	
--- src/lab/Runner.java
+++ src/lab/Runner.java
@@ -1,23 +1,67 @@
 package lab;
 
+import java.util.Scanner;
+
 public class Runner {
 
 	public static void main(String[] args) {
 		
 		ArrayAnalysis analysis = new ArrayAnalysis();
 		
-		int[][] arr1 = analysis.createRandom(6, 3, -2, 10);
+		int[][] arr1 = analysis.createRandom(6, 3, -100, 100);
 		
+		int[][] arr2 = analysis.createRandom(6, 3, 0, 1000);
 		
+		//printing both arrays
 		
+		analysis.printArray(arr1);
+		System.out.println();
+		analysis.printArray(arr2);
+		System.out.println();
 		
+		//method 3
+		System.out.print("Arr1 Total: ");
+		System.out.println(analysis.sumTotal(arr1));
+		System.out.print("Arr2 Total: ");
+		System.out.println(analysis.sumTotal(arr2));
+		//method 4
+		System.out.print("\nArr1 AVG: ");
+		System.out.println(analysis.average(arr1));
+		System.out.print("Arr2 AVG: ");
+		System.out.println(analysis.average(arr2));
+		//method 5
+		System.out.print("\nArr1 Min: ");
+		System.out.println(analysis.findMin(arr1));
+		System.out.print("Arr2 Min: ");
+		System.out.println(analysis.findMin(arr2));
+		//method 6
+		System.out.print("\nArr1 Max: ");
+		System.out.println(analysis.findMax(arr1));
+		System.out.print("Arr2 Max: ");
+		System.out.println(analysis.findMax(arr2)+"\n");
+		//method 7
+		System.out.println("The diffence beetween the arrays:");
+		analysis.printArray(analysis.diffArrays(arr1, arr2));
+		// method 8
+		analysis.printArray(analysis.transposeRow(arr1,getInt("How many rows to move?")));
+			
 		
+		// mr hunter code
+		int[][][] arr3D = new int[3][4][5];
+		for (int i = 0; i < arr3D.length; i++) {
+			arr3D[i] = analysis.createRandom(4, 5, -400, 400);
+		}
+		int x = 1;
+		for (int[][] arr : arr3D) {
+			System.out.println("\nLayer #" + x);
+			analysis.printArray(arr);
+			x++;
+		}
 		
-		
-		
-		
-		
-		
+		//benedict code
+		System.out.println("\narr3D sum: " + analysis.sum3D(arr3D));
+		// method 10 avrage
+		System.out.println("\narr3D AVG: " + analysis.average3D(arr3D));
 	}
 	
 	
@@ -26,4 +70,15 @@ public class Runner {
 	
 	
 	
+
+
+
+
+
+
+	public static int getInt(String print){
+		System.out.println("\n"+print);
+		Scanner inKey = new Scanner(System.in);
+		return inKey.nextInt();
+	}
 }
